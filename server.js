844d9a2bab173762b5062cfc2ce1d07597330b98
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-02-27 17:13:53 -0600

    initial commit w/ skeleton from Phil. added Mongoose handling

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+var routes= require("./controllers/routes.js");
+
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+// // Connect to the Mongo DB
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
+
